a6946c52bfef02b3ac0ed0ffe55ad392a2f4e68d
knkp <knkp934@gmail.com>
2017-11-30 00:23:01 -0500

    added arduino firmware, on this relay, connect the fan to NO not NC

---
+++ arduino/PiAlarmFirmware/PiAlarmFirmware.ino
@@ -0,0 +1,22 @@
+#define RELAY_CTL_PIN 7
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(RELAY_CTL_PIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char Message = Serial.read();
+  if(Message == 'a')
+  {
+    Serial.println("on");
+    digitalWrite(RELAY_CTL_PIN, HIGH);
+  }
+  if(Message == 'b')
+  {
+    Serial.println("off");
+    digitalWrite(RELAY_CTL_PIN, LOW);
+  }
+}
